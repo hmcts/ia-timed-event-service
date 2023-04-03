150c0eb80f8a14449275588bde2f36d28688314a
colmlynch60 <colm.lynch@hmcts.net>
2023-04-03 13:20:32 +0100

    Updated dependencies to remove suppressed CVEs

--- src/integrationTest/java/uk/gov/hmcts/reform/timedevent/infrastructure/controllers/support/GetTokenIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/timedevent/infrastructure/controllers/support/GetTokenIntegrationTest.java
@@ -4,19 +4,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.github.tomakehurst.wiremock.WireMockServer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.web.servlet.MvcResult;
-import ru.lanwen.wiremock.ext.WiremockResolver;
 import uk.gov.hmcts.reform.timedevent.testutils.SpringBootIntegrationTest;
-import uk.gov.hmcts.reform.timedevent.testutils.StaticPortWiremockFactory;
 import uk.gov.hmcts.reform.timedevent.testutils.WithIdamStub;
 
 public class GetTokenIntegrationTest extends SpringBootIntegrationTest implements WithIdamStub {
 
     @BeforeEach
-    public void stubRequests(@WiremockResolver.Wiremock(factory = StaticPortWiremockFactory.class) WireMockServer server) {
+    public void stubRequests() {
 
         addIdamTokenStub(server);
     }
