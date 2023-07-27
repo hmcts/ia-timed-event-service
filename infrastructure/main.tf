57b10b96696f8aec2f0fbfded8f6fd7680783495
Filippo Possenti <filippo.possenti@version1.com>
2023-07-27 16:51:44 +0100

    RIA-7317 Removed unnecessary Terraform change

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -32,14 +32,3 @@ resource "azurerm_key_vault_secret" "POSTGRES-PASS-11" {
   value        = "${module.ia_timed_event_service_database_11.postgresql_password}"
   key_vault_id = "${data.azurerm_key_vault.ia_key_vault.id}"
 }
-
-data "azurerm_key_vault_secret" "app_insights_connection_string" {
-  name      = "ia-app-insights-connection-string"
-  key_vault_id = data.azurerm_key_vault.ia_key_vault.id
-}
-
-resource "azurerm_key_vault_secret" "local_app_insights_connection_string" {
-  name         = "app-insights-connection-string"
-  value        = data.azurerm_key_vault_secret.app_insights_connection_string.value
-  key_vault_id = data.azurerm_key_vault.ia_key_vault.id
-}
