bf9c15368b6ee412821025a242cbccf4f226fa35
Filippo Possenti <filippo.possenti@version1.com>
2023-09-06 11:46:43 +0100

    RIA-7757 Integration tests on retry logic

---
+++ src/integrationTest/java/uk/gov/hmcts/reform/timedevent/infrastructure/controllers/RetryLogicIntegrationTest.java
@@ -0,0 +1,130 @@
+package uk.gov.hmcts.reform.timedevent.infrastructure.controllers;
+
+import java.time.ZonedDateTime;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import feign.FeignException;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MvcResult;
+import uk.gov.hmcts.reform.timedevent.infrastructure.domain.entities.TimedEvent;
+import uk.gov.hmcts.reform.timedevent.infrastructure.domain.entities.ccd.Event;
+import uk.gov.hmcts.reform.timedevent.infrastructure.domain.services.EventExecutor;
+import uk.gov.hmcts.reform.timedevent.testutils.SpringBootIntegrationTest;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class RetryLogicIntegrationTest extends SpringBootIntegrationTest {
+
+    @MockBean
+    EventExecutor eventExecutor;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    @WithMockUser(authorities = {"caseworker-ia-caseofficer"})
+    void testScheduledEventHasRunAfterAppropriateTime() {
+        // Given: an event scheduled in the future
+        scheduleEvent(ZonedDateTime.now().plusSeconds(5));
+
+        // When: I wait for enough time to pass
+        weirdSleep(6000);
+
+        // Then: the event is executed
+        verify(eventExecutor, times(1)).execute(any());
+    }
+
+    @Test
+    @WithMockUser(authorities = {"caseworker-ia-caseofficer"})
+    void testScheduledEventHasNotRunBeforeTime() {
+        // Given: an event scheduled in the future
+        scheduleEvent(ZonedDateTime.now().plusSeconds(5));
+
+        // When: I don't wait for enough time to pass
+        weirdSleep(1000);
+
+        // Then: the event is not executed
+        verify(eventExecutor, times(0)).execute(any());
+    }
+
+    @SneakyThrows
+    @Test
+    @WithMockUser(authorities = {"caseworker-ia-caseofficer"})
+    void testRetryableExecutionFailureTriggersAnotherAttempt() {
+        // Given: an event scheduled in the future that is destined to fail
+        doThrow(FeignException.GatewayTimeout.class).when(eventExecutor).execute(any());
+
+        scheduleEvent(ZonedDateTime.now().plusSeconds(5));
+
+        // When: I wait for enough time to pass
+        weirdSleep(30000);
+
+        // Then: the event execution is attempted at least twice
+        verify(eventExecutor, atLeast(2)).execute(any());
+    }
+
+    @Test
+    @WithMockUser(authorities = {"caseworker-ia-caseofficer"})
+    void testExecutionFailureMaximumAttemptLimitIsRespected() {
+        // Given: an event scheduled in the future that is destined to fail
+        doThrow(FeignException.GatewayTimeout.class).when(eventExecutor).execute(any());
+
+        scheduleEvent(ZonedDateTime.now().plusSeconds(5));
+
+        // When: I wait for enough time to pass
+        weirdSleep(30000);
+
+        // Then: the event execution is attempted exactly three times
+        verify(eventExecutor, times(3)).execute(any());
+    }
+
+    @SneakyThrows
+    private TimedEvent scheduleEvent(ZonedDateTime scheduledDateTime) {
+        MvcResult postResponse = mockMvc
+            .perform(
+                post("/timed-event")
+                    .content(buildTimedEvent(scheduledDateTime))
+                    .contentType("application/json")
+            )
+            .andExpect(status().isCreated())
+            .andReturn();
+
+        TimedEvent result = objectMapper.readValue(postResponse.getResponse().getContentAsString(), TimedEvent.class);
+        return result;
+    }
+
+    /**
+     * This is needed for the test as unfortunately without it the tests sometimes fail as if Quartz is running on
+     * the same thread as the sleep instruction
+     * @param totalMillis The total wait time
+     */
+    @SneakyThrows
+    private void weirdSleep(int totalMillis) {
+        int total = 0;
+        final int INCREMENT = 500;
+
+        while(total < totalMillis) {
+            Thread.sleep(INCREMENT);
+            total += INCREMENT;
+        }
+
+    }
+
+    @SneakyThrows
+    private String buildTimedEvent(ZonedDateTime scheduledDateTime) {
+        //scheduledDateTime.format(DateTimeFormatter.ISO_DATE_TIME)
+        TimedEvent timedEvent = new TimedEvent(null, Event.EXAMPLE, scheduledDateTime, "IA", "Asylum", 1588772172174023L);
+        return objectMapper.writeValueAsString(timedEvent);
+    }
+
+}
