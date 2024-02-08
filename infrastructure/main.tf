79ba6661049c93c82a4c3d9a29b3f99adc52494c
KleoG <KleoG@users.noreply.github.com>
2024-02-08 14:23:06 +0000

    DIAC-158 decommission postgres test (#166)
    * DIAC-158 - decommission postgres 11
    
    * DIAC-158 - force delete resource groups
    
    * DIAC-158 - force delete resource groups
    
    * DIAC-158 - remove force delete resource groups from new dbs
    
    ---------
    
    Co-authored-by: alivenichoppa <97441560+alivenichoppa@users.noreply.github.com>

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,13 +1,13 @@
 provider "azurerm" {
-  features {}
+    features {
+        resource_group {
+            prevent_deletion_if_contains_resources = false
+        }
+    }
 }
 
 provider "azurerm" {
-  features {
-    resource_group {
-        prevent_deletion_if_contains_resources = false
-    }
-  }
+  features {}
   skip_provider_registration = true
   alias                      = "cft_vnet"
   subscription_id            = var.aks_subscription_id
