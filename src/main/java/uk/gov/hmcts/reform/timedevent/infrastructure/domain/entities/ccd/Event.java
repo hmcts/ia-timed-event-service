35ae3635211456253360b8e867ee5e8273a3b8c5
jamieclarkeHMCTS <128475668+jamieclarkeHMCTS@users.noreply.github.com>
2024-04-25 13:58:03 +0100

    RIA-8064_timed_event_permissions (#171)
    Co-authored-by: JamieClarkeV1 <120718578+JamieClarkeV1@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/timedevent/infrastructure/domain/entities/ccd/Event.java
+++ src/main/java/uk/gov/hmcts/reform/timedevent/infrastructure/domain/entities/ccd/Event.java
@@ -16,6 +16,7 @@ public enum Event {
     ROLLBACK_PAYMENT_TIMEOUT_TO_PAYMENT_PENDING("rollbackPaymentTimeoutToPaymentPending"),
     END_APPEAL_AUTOMATICALLY("endAppealAutomatically"),
     RE_TRIGGER_WA_TASKS("reTriggerWaTasks"),
+    RECORD_REMISSION_REMINDER("recordRemissionReminder"),
     @JsonEnumDefaultValue
     UNKNOWN("unknown");
 
