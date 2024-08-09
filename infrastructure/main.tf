956a90ea956ce773c6d1c2e47dfcb5e5d5d47d3f
ghs_VFQtN1YaxHCEfLZMUVbpy65LI6gF6c3aujMx <62423932+hmcts-jenkins-d-to-i[bot]@users.noreply.github.com>
2024-08-09 11:19:26 +0000

    Updating Terraform Formatting

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,9 +1,9 @@
 provider "azurerm" {
-    features {
-        resource_group {
-            prevent_deletion_if_contains_resources = false
-        }
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
     }
+  }
 }
 
 provider "azurerm" {
@@ -15,9 +15,9 @@ provider "azurerm" {
 
 locals {
 
-  preview_vault_name           = "${var.raw_product}-aat"
-  non_preview_vault_name       = "${var.raw_product}-${var.env}"
-  key_vault_name               = var.env == "preview" || var.env == "spreview" ? local.preview_vault_name : local.non_preview_vault_name
+  preview_vault_name     = "${var.raw_product}-aat"
+  non_preview_vault_name = "${var.raw_product}-${var.env}"
+  key_vault_name         = var.env == "preview" || var.env == "spreview" ? local.preview_vault_name : local.non_preview_vault_name
 
 }
 
@@ -30,13 +30,13 @@ module "ia-timed-event-service-db-v15" {
   providers = {
     azurerm.postgres_network = azurerm.cft_vnet
   }
-  source          = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
-  env             = var.env
-  product         = var.product
-  component       = var.component
-  business_area   = "cft"
-  common_tags     = merge(var.common_tags, tomap({"lastUpdated" = "${timestamp()}"}))
-  name            = "${var.product}-${var.component}-postgres-db-v15"
+  source        = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
+  env           = var.env
+  product       = var.product
+  component     = var.component
+  business_area = "cft"
+  common_tags   = merge(var.common_tags, tomap({ "lastUpdated" = "${timestamp()}" }))
+  name          = "${var.product}-${var.component}-postgres-db-v15"
   pgsql_databases = [
     {
       name : var.postgresql_database_name
@@ -48,15 +48,15 @@ module "ia-timed-event-service-db-v15" {
       value = "plpgsql,pg_stat_statements,pg_buffercache"
     }
   ]
-  pgsql_version   = "15"
-  subnet_suffix = "expanded"
-  admin_user_object_id = var.jenkins_AAD_objectId
+  pgsql_version                  = "15"
+  subnet_suffix                  = "expanded"
+  admin_user_object_id           = var.jenkins_AAD_objectId
   force_user_permissions_trigger = "1"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES-PASS-15" {
   name         = "${var.component}-POSTGRES-PASS-15"
-  value        = "${module.ia-timed-event-service-db-v15.password}"
-  key_vault_id = "${data.azurerm_key_vault.ia_key_vault.id}"
+  value        = module.ia-timed-event-service-db-v15.password
+  key_vault_id = data.azurerm_key_vault.ia_key_vault.id
 }
 
