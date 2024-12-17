8e523d7ac4af0456d3776c8da05803f6d5b8522e
alivenichoppa <97441560+alivenichoppa@users.noreply.github.com>
2024-12-17 14:14:07 +0000

    Update RetryLogicIntegrationTest.java

--- src/integrationTest/java/uk/gov/hmcts/reform/timedevent/infrastructure/controllers/RetryLogicIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/timedevent/infrastructure/controllers/RetryLogicIntegrationTest.java
@@ -7,7 +7,6 @@ import feign.FeignException;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.quartz.Scheduler;
@@ -66,7 +65,6 @@ public class RetryLogicIntegrationTest extends SpringBootIntegrationTest {
     }
 
     @Test
-    @Disabled
     @WithMockUser(authorities = {"caseworker-ia-caseofficer"})
     void testScheduledEventHasRunAfterAppropriateTime() {
         int maxAttempts = 10;
