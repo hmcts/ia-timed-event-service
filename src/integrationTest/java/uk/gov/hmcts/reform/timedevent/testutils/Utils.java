c6380fb84bcc3d91f4fb46d5bee5134d15f21ba5
nilay <nilay.patel@solirius.com>
2025-11-11 15:13:59 +0000

    checsktyle fix

--- src/integrationTest/java/uk/gov/hmcts/reform/timedevent/testutils/Utils.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/timedevent/testutils/Utils.java
@@ -4,6 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Utils {
+    private Utils() {
+    }
+
     public static void retryTestCodeBlock(int maxAttempts, Runnable codeBlock) {
         Throwable finalError = null;
         for (int i = 0; i < maxAttempts; i++) {
