6078bb21b81e48e7b81a7e0850d7ab1235f3d5b7
nilay <nilay.patel@solirius.com>
2026-01-23 16:27:40 +0000

    adding saveNotificationsToDataBail

--- src/main/java/uk/gov/hmcts/reform/timedevent/domain/entities/ccd/Event.java
+++ src/main/java/uk/gov/hmcts/reform/timedevent/domain/entities/ccd/Event.java
@@ -19,6 +19,7 @@ public enum Event {
     RECORD_REMISSION_REMINDER("recordRemissionReminder"),
     SEND_PAYMENT_REMINDER_NOTIFICATION("sendPaymentReminderNotification"),
     SAVE_NOTIFICATIONS_TO_DATA("saveNotificationsToData"),
+    SAVE_NOTIFICATIONS_TO_DATA_BAIL("saveNotificationsToDataBail"),
     TEST_TIMED_EVENT_SCHEDULE("testTimedEventSchedule"),
     @JsonEnumDefaultValue
     UNKNOWN("unknown");
